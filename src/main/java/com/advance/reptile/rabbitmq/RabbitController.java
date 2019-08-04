b2cd4e0289cbb725694feaec1fcd21e1da099ede
advance1997 <2497947721@qq.com>
2019-08-05 01:32:14 +0800

    新增爬虫简单模式与复杂模式接口，通过rabbitMq进行小说爬取请求处理

--- src/main/java/com/advance/reptile/rabbitmq/RabbitController.java
+++ src/main/java/com/advance/reptile/rabbitmq/RabbitController.java
@@ -2,9 +2,7 @@ package com.advance.reptile.rabbitmq;
 
 import com.advance.reptile.common.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +22,16 @@ public class RabbitController {
         messagePojo.setParams(param);
         messagePojo.setClassName("StrategyA");
         messageProvider.sendMessage(messagePojo);
+        return Response.success();
+    }
+
+    @RequestMapping(value = "/scrpy/simple", method = RequestMethod.POST)
+    public Response scrpyBookSimple(@RequestBody String data){
+        return Response.success();
+    }
+
+    @RequestMapping(value = "/scrpy/master", method = RequestMethod.POST)
+    public Response scrpyBookMaster(@RequestBody String data){
         return Response.success();
     }
 
