03a150daa51feeafa24f74947a0cc4b4b9f24ff4
advance <2497947721@qq.com>
2019-08-19 18:07:44 +0800

    添加netty服务启动

--- src/main/java/com/advance/reptile/ReptileApplication.java
+++ src/main/java/com/advance/reptile/ReptileApplication.java
@@ -1,6 +1,8 @@
 package com.advance.reptile;
 
+import com.advance.reptile.socket.NettyService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @Configuration
-public class ReptileApplication {
+public class ReptileApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReptileApplication.class, args);
 	}
 
+	//实现CommandLineRunner 重写run方法 这里放了netty的启动
+	@Override
+	public void run(String... args) throws Exception {
+		new NettyService();
+	}
 }
